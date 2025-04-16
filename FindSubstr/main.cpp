d90a7c4664246555723fa1973d4331ce37b51d78
Maleee5835 <bulut.mali@hotmail.com>
2025-04-16 19:05:28 +0300

    .
    find substr index

--- FindSubstr/main.cpp
+++ FindSubstr/main.cpp
@@ -5,21 +5,37 @@ using namespace std;
 int main()
 {
     string haystack, needle;
-    cout << "Haystack string'ini girin: ";
+
+    cout << "Enter the haystack string: ";
     getline(cin, haystack);
-    cout << "Needle string'ini girin: ";
+    cout << "Enter the needle string: ";
     getline(cin, needle);
+
     cout << "Haystack: " << haystack << endl;
     cout << "Needle: " << needle << endl;
-    vector<int> vec;
-    int h = haystack.size(), n = needle.size();
-    if (n == 0) return 0;
-    for (int i = 0; i <= h - n; i++) {
-    if (haystack.substr(i, n) == needle)
-            vec.push_back(i);
+
+    vector<int> indices;
+    int haystackLength = haystack.size(), needleLength = needle.size();
+
+    if (needleLength == 0) return 0; // If needle is empty, exit
+
+    // Loop to find all occurrences of the needle in the haystack
+    for (int i = 0; i <= haystackLength - needleLength; i++) {
+        if (haystack.substr(i, needleLength) == needle) {
+            indices.push_back(i); // Store the index where needle occurs
+        }
     }
-    for(int i=0;i<vec.size();i++){
-        cout << vec[i] << " ";
+
+    // Print the indices where the needle was found
+    if (indices.empty()) {
+        cout << "No occurrences found." << endl;
+    } else {
+        cout << "Needle found at indices: ";
+        for (int i = 0; i < indices.size(); i++) {
+            cout << indices[i] << " ";
+        }
+        cout << endl;
     }
+
     return 0;
 }
